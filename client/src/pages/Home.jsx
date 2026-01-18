965c183d0af21438518f0001024b8b48e323316f
leo <leonardocruzado8060@gmail.com>
2026-01-18 15:21:12 -0500

    Update Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="page">
-      <h1>Welcome to MERN Stack App (TESTING)</h1>
+      <h1>Welcome to MERN Stack App (TESTING) testing 123</h1>
       <p>This is the home page.</p>
     </div>
   );
