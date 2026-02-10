78f433cae5e4194e46c46d39821d19a0ed8ca4ff
1leo1tro <leonardocruzado8060@gmail.com>
2026-02-09 23:47:59 -0600

    feat: update listing view after successful edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import PropertyDetails from './pages/PropertyDetails';
 import SignIn from './pages/SignIn';
 import Listings from './pages/Listings';
 import CreateListing from './pages/CreateListing';
+import EditListing from './pages/EditListing';
 import Navbar from './components/Navbar';
 import './styles/App.css';
 import './styles/Navbar.css';
@@ -27,6 +28,7 @@ function App() {
               <Route path="/signin" element={<SignIn />} />
               <Route path="/listings" element={<Listings />} />
               <Route path="/listings/create" element={<CreateListing />} />
+              <Route path="/listings/edit/:id" element={<EditListing />} />
             </Routes>
           </main>
         </div>
