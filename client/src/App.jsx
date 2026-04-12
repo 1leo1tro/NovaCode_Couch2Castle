3bf950a41199b2cebd1cc083d57a077e12847807
JoshuaTLogue <111827108+JoshuaTLogue@users.noreply.github.com>
2026-04-11 23:46:26 -0500

    DARK MODE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 import Home from './pages/Home';
 import Contacts from './pages/Contacts';
@@ -12,7 +13,6 @@ import CreateListing from './pages/CreateListing';
 import EditListing from './pages/EditListing';
 import Showings from './pages/Showings';
 import Help from './pages/Help';
-import CreateUser from './pages/CreateUser';
 import Reports from './pages/Reports';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -24,41 +24,42 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="app">
-          <Navbar />
-          <main className="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/help" element={<Help />} />
-              <Route path="/property/:id" element={<PropertyDetails />} />
-              <Route path="/signin" element={<SignIn />} />
-              <Route path="/create-user" element={<CreateUser />} />
-              <Route path="/listings" element={<Listings />} />
-              <Route path="/listings/mine" element={
-                <ProtectedRoute requireAgent={true}>
-                  <MyListings />
-                </ProtectedRoute>
-              } />
-              <Route path="/listings/create" element={
-                <ProtectedRoute requireAgent={true}>
-                  <CreateListing />
-                </ProtectedRoute>
-              } />
-              <Route path="/listings/edit/:id" element={<EditListing />} />
-              <Route path="/showings" element={
-                <ProtectedRoute requireAgent={true}>
-                  <Showings />
-                </ProtectedRoute>
-              } />
-              <Route path="/reports" element={
-                <ProtectedRoute requireAgent={true}>
-                  <Reports />
-                </ProtectedRoute>
-              } />
-            </Routes>
-          </main>
-        </div>
+        <ThemeProvider>
+          <div className="app">
+            <Navbar />
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/help" element={<Help />} />
+                <Route path="/property/:id" element={<PropertyDetails />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/listings" element={<Listings />} />
+                <Route path="/listings/mine" element={
+                  <ProtectedRoute requireAgent={true}>
+                    <MyListings />
+                  </ProtectedRoute>
+                } />
+                <Route path="/listings/create" element={
+                  <ProtectedRoute requireAgent={true}>
+                    <CreateListing />
+                  </ProtectedRoute>
+                } />
+                <Route path="/listings/edit/:id" element={<EditListing />} />
+                <Route path="/showings" element={
+                  <ProtectedRoute requireAgent={true}>
+                    <Showings />
+                  </ProtectedRoute>
+                } />
+                <Route path="/reports" element={
+                  <ProtectedRoute requireAgent={true}>
+                    <Reports />
+                  </ProtectedRoute>
+                } />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
       </AuthProvider>
     </Router>
   );
